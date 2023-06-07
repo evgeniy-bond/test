17f05d0ecb5f8b06d1fc63fde8a1e49c87f46cb3
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2023-06-07 23:00:51 +0300

    chore: update

--- src/app/utils/provider.tsx
+++ src/app/utils/provider.tsx
@@ -1,11 +1,5 @@
 import { AnkrProvider } from '@ankr.com/ankr.js';
 
-// This is public url
-const DEFAULT_RPC_ENDPOINT =
-  'https://rpc.ankr.com/multichain/249e6156637f0fd30fd92242ec1739e49a176a723f3894c4ae1e299c2fad9635';
-
-const provider = new AnkrProvider(
-  process.env.RPC_ENDPOINT || DEFAULT_RPC_ENDPOINT,
-);
+const provider = new AnkrProvider(process.env.RPC_ENDPOINT || '');
 
 export default provider;
